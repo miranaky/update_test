d91bbbb752de7d354a877a33d7b76372b0113400
sungmuk kang <miranaky@gmail.com>
2022-07-21 15:51:42 +0900

    feat: mul function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from add import add
 from sub import sub
+from mul import mul
 
 def main():
     print(f'{add(1,2)=}')
     print(f'{sub(2,1)=}')
+    print(f'{mul(2,2)=}')
 
 if __name__=="__main__":
     main()
